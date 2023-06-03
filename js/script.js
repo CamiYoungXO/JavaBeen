5c03b3957467899f9083e5077e58a6b2e4cb2f20
Cameron Young <youngcameron71@gmail.com>
2023-06-03 16:14:27 -0500

    New Validations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+function validate()
+{
+  var username=document.getElementById("username").value;
+  var password=document.getElementById("password").value;
+
+  if(username=="admin" && password=="user")
+  {
+    alert("login successful");
+    return false;
+  }
+  else
+  {
+    alert("login failed");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/* for img fade in
 $(window).load(function () {
     $('#img1').fadeIn(3000);
   });
+  */
